e908c68e459c3575da5128d4a7e6e319006bc339
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-03-01 17:29:40 +0100

    Create Admin users account

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $newUser = new User();
+        $newUser->name = 'Admin';
+        $newUser->email = 'admin@admin.com';
+        $newUser->password = Hash::make('12345678');
+        $newUser->save();
+    }
+}
